a22763c27333e307f8e400c486df87aaab8581ee
Yuri Moreira Leocadio <yurileocadio0411@gmail.com>
2024-07-23 19:35:24 -0300

    first commit

---
+++ src/main/java/com/api/aula_crud/AulaCrudApplication.java
@@ -0,0 +1,13 @@
+package com.api.aula_crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/aula_crud/AulaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.aula_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
